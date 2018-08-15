d9726c244794ddbb029f7427e1db2a5cd3e55b2a
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-15 11:10:11 -0700

    Added routing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,29 @@ import Header from './components/layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from './context';
 import AddContact from './components/contacts/AddContact';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import EditContact from './components/contacts/EditContact';
 
 class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          <Header />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            <Header />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/add" component={AddContact} />
+                <Route exact path="/edit/:id" component={EditContact} />
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
